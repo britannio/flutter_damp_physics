7be84867a470c2b277763f7aa6e1fe7b16a3a7eb
Britannio <britanniojarrett@gmail.com>
2020-12-25 22:06:33 +0000

    initial commit

---
+++ lib/damp_physics.dart
@@ -0,0 +1,80 @@
+library damp_physics;
+
+import 'package:flutter/material.dart';
+
+/// Disables the bouncing underscroll effect present in IOS's ClampingScrollPhysics
+class NoBouncingUnderscrollPhysics extends ScrollPhysics {
+  const NoBouncingUnderscrollPhysics({
+    ScrollPhysics parent,
+  }) : super(parent: parent);
+
+  @override
+  NoBouncingUnderscrollPhysics applyTo(ScrollPhysics ancestor) {
+    return NoBouncingUnderscrollPhysics(parent: buildParent(ancestor));
+  }
+
+  @override
+  double applyBoundaryConditions(ScrollMetrics position, double value) {
+    // Inspired by ClampingScrollPhysics.applyBoundaryConditions
+    if (value < position.pixels &&
+        position.pixels <= position.minScrollExtent) {
+      // Underscroll
+      return value - position.pixels;
+    }
+    if (position.maxScrollExtent <= position.pixels &&
+        position.pixels < value) {
+      // Overscroll
+
+      return parent.applyBoundaryConditions(position, value);
+    }
+    if (value < position.minScrollExtent &&
+        position.minScrollExtent < position.pixels) {
+      // Hit top edge
+      return value - position.minScrollExtent;
+    }
+    if (position.pixels < position.maxScrollExtent &&
+        position.maxScrollExtent < value) {
+      // Hit bottom edge
+      return parent.applyBoundaryConditions(position, value);
+    }
+    return 0.0;
+  }
+}
+
+/// Disables the bouncing overscroll effect present in IOS's ClampingScrollPhysics
+class NoBouncingOverscrollPhysics extends ScrollPhysics {
+  const NoBouncingOverscrollPhysics({ScrollPhysics parent})
+      : super(parent: parent);
+
+  @override
+  NoBouncingOverscrollPhysics applyTo(ScrollPhysics ancestor) {
+    return NoBouncingOverscrollPhysics(parent: buildParent(ancestor));
+  }
+
+  @override
+  double applyBoundaryConditions(ScrollMetrics position, double value) {
+    // Inspired by ClampingScrollPhysics.applyBoundaryConditions
+    if (value < position.pixels &&
+        position.pixels <= position.minScrollExtent) {
+      // Underscroll
+      return parent.applyBoundaryConditions(position, value);
+    }
+    if (position.maxScrollExtent <= position.pixels &&
+        position.pixels < value) {
+      // Overscroll
+
+      return value - position.pixels;
+    }
+    if (value < position.minScrollExtent &&
+        position.minScrollExtent < position.pixels) {
+      // Hit top edge
+      return parent.applyBoundaryConditions(position, value);
+    }
+    if (position.pixels < position.maxScrollExtent &&
+        position.maxScrollExtent < value) {
+      // Hit bottom edge
+      return value - position.maxScrollExtent;
+    }
+    return 0.0;
+  }
+}
